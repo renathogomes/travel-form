37dfe82501879c47030d0181c2c45ecc727aa04a
renathogomes <renatho.20gomes@gmail.com>
2023-04-26 10:55:08 -0300

    sexto item

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const btnSubmit = document.querySelector('#submit-btn');
-btnSubmit.addEventListener('click', (e) => {
-  e.preventDefault();
-});
+
+const verificaCheck = () => {
+  const checado = document.getElementById('agreement');
+    if (checado) {
+      const botao = document.createElement('button');
+      const formulario = document.querySelector('#main-form');
+      formulario.appendChild(botao);
+      botao.id = 'submit-btn';
+      botao.innerText = 'Enviar';
+      botao.type = 'submit';
+    }
+    const btnSubmit = document.querySelector('#submit-btn');
+    btnSubmit.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+}
+
+verificaCheck();
+
+
+console.log(typeof checado)
