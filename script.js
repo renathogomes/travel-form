b9d51166845dee96666341c578484b528d495f7e
renathogomes <renatho.20gomes@gmail.com>
2023-04-26 11:04:10 -0300

    lintando

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-
 const verificaCheck = () => {
   const checado = document.getElementById('agreement');
-    if (checado) {
-      const botao = document.createElement('button');
-      const formulario = document.querySelector('#main-form');
-      formulario.appendChild(botao);
-      botao.id = 'submit-btn';
-      botao.innerText = 'Enviar';
-      botao.type = 'submit';
-    }
-    const btnSubmit = document.querySelector('#submit-btn');
-    btnSubmit.addEventListener('click', (e) => {
-      e.preventDefault();
-    });
-}
+  if (checado) {
+    const botao = document.createElement('button');
+    const formulario = document.querySelector('#main-form');
+    formulario.appendChild(botao);
+    botao.id = 'submit-btn';
+    botao.innerText = 'Enviar';
+    botao.type = 'submit';
+  }
+  const btnSubmit = document.querySelector('#submit-btn');
+  btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+};
 
 verificaCheck();
-
-
-console.log(typeof checado)
